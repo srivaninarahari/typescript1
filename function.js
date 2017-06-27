532c48d2e45f923783f0409d212c457a0a34bf60
srivaninarahari <srivaninarahari18@gmail.com>
2017-06-27 14:22:36 +0530

    sree

---
+++ function.js
@@ -0,0 +1,5 @@
+//lamba  functon  which give  string  and  number--//
+var studentName = function (name, age) { console.log("the student  name  is  " + name + " and  age  is " + age + " "); };
+var studentName1 = function (name, age) { alert("the student  name  is  " + name + " and  age  is " + age + " "); };
+studentName('sreevani', 25);
+studentName1('sreevani', 25);
---
+++ function.ts
@@ -0,0 +1,10 @@
+
+//lamba  functon  which give  string  and  number--//
+
+let  studentName = (name:string, age:number) =>{ console.log(`the student  name  is  ${name } and  age  is ${age} `) }
+
+ let studentName1 = (name:string, age:number)=>{ alert(`the student  name  is  ${name } and  age  is ${age} `)  }
+
+
+studentName ('sreevani',25)
+studentName1('sreevani',25)
